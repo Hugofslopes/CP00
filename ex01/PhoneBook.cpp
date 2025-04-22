010a9f08962f996ee29fa3fa39977dad9d3c3399
Hugo Lopes <hugofslopes@live.com.pt>
2025-04-22 20:30:46 +0100

    finishing ex01

--- ex01/PhoneBook.cpp
+++ ex01/PhoneBook.cpp
@@ -6,42 +6,68 @@
 /*   By: hfilipe- <hfilipe-@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/22 15:44:50 by hfilipe-          #+#    #+#             */
-/*   Updated: 2025/04/22 16:35:43 by hfilipe-         ###   ########.fr       */
+/*   Updated: 2025/04/22 20:29:19 by hfilipe-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # include "PhoneBook.hpp"
 
-void	add_contacts(){
-	std::string name, last_name, nickname;
+void	add_contacts(PhoneBook *phonebook){
+	
+	std::string name, lastName, nickName;
 	std::cout << "Enter the contact Name" << std::endl;
 	std::getline(std::cin, name);
 	std::cout << "Enter the contact Last Name" << std::endl;
-	std::getline(std::cin, last_name);
+	std::getline(std::cin, lastName);
 	std::cout << "Enter the contact Nickname" << std::endl;
-	std::getline(std::cin, name);
+	std::getline(std::cin, nickName);
+	Contact contact(name, lastName, nickName);
+	(*phonebook).setContact(contact);
 }
 
-void	printPhoneBook(){
-	for (size_t i = 0; i < 10; i++)
-		std::cout << "ola";
-}
-
-int main()
+void	display_ct_info(PhoneBook *phonebook)
 {
+	std::string input;
+	int number;
+
+	(*phonebook).printContacts();
+	std::cout << "Please select the index for the specific contact";
+    std::getline(std::cin, input);
+    try {
+        number = std::stoi(input);
+    } 
+	catch (const std::invalid_argument& e) {
+        std::cout << "Invalid input. Please enter a valid integer." << std::endl;
+    }
+	catch (const std::out_of_range& e) {
+        std::cout << "The number is out of range." << std::endl;
+    }
+	if (number < 0 || number > 7 )
+		std::cout << "Invalid input. Please enter a valid index" << std::endl;
+	phonebook->printContact(number);
+}
+
+int	main()
+{
+	PhoneBook phonebook;
+	
 	while (1)
 	{
 		std::string in;
-		std::cout << "Enter one of the fowloing commands:\nADD- To add new contact\n\
-		SEARCH - Show the list of contacts\nEXIT- Exit the progam" << std::endl;
+		
+		std::cout << "Enter one of the fowloing commands:"<< std::endl;
+		PRINT_GREEN("<ADD> - Add new contact");
+		PRINT_BLUE("<SEARCH> - Show the list of contacts");
+		PRINT_RED("<EXIT> - Exit the progam");
 		std::getline(std::cin, in);
 		if (in == "EXIT")
 			exit(0);
 		else if (in == "ADD")
-			add_contacts();
+			add_contacts(&phonebook);
 		else if (in == "SEARCH")
-			printPhoneBook();
+			display_ct_info(&phonebook);
 		else
-			std::cout << "Wrong input" << std::endl;
+			std::cout <<BOLD("				WRONG INPUT") << std::endl;
 	}
 }
+
--- ex01/PhoneBook.hpp
+++ ex01/PhoneBook.hpp
@@ -4,22 +4,120 @@
 # include <iostream>
 # include <cstring>
 # include <string>
-/* 
-class PhoneBook {
-	private:
-		int Contacts[8];
+# include <iomanip>
 
-};
+#define RESET   "\033[0m"
+#define GREEN   "\033[32m"
+#define RED     "\033[31m"
+#define BLUE    "\033[34m"
 
-class Contacts {
+#define PRINT_GREEN(text) std::cout << GREEN << text << RESET << std::endl
+#define PRINT_RED(text) std::cout << RED << text << RESET << std::endl
+#define PRINT_BLUE(text) std::cout << BLUE << text << RESET << std::endl
+#define BOLD(text) "\033[1m" text "\033[0m"
+
+class Contact {
+
 	public:
-	
+		int	index;
+		Contact(){}
+		Contact(std::string fN, std::string lN, std::string	nN){
+			firstName = fN;
+			lastName = lN;
+			nickName = nN;
+		}
+
+		void display() const{
+			const int columnWidth = 10;
+
+			std::string truncatedFirstName = firstName.length() > columnWidth ? firstName.substr(0, columnWidth - 1) + "." : firstName;
+			std::string truncatedLastName = lastName.length() > columnWidth ? lastName.substr(0, columnWidth - 1) + "." : lastName;
+			std::string truncatedNickName = nickName.length() > columnWidth ? nickName.substr(0, columnWidth - 1) + "." : nickName;
+			std::cout << std::setw(columnWidth) << std::right << index \
+			<< "|" << std::setw(columnWidth) << std::right << truncatedFirstName \
+			<< "|" << std::setw(columnWidth) << std::right << truncatedLastName \
+			<< "|" << std::setw(columnWidth) << std::right << truncatedNickName << std::endl;
+		}
+
+		void displayHeader() const{
+			const int columnWidth = 10;
+			std::cout << std::setw(columnWidth) << std::right << BOLD("     Index") \
+			<< "|" << std::setw(columnWidth) << std::right << BOLD("First Name") \
+			<< "|" << std::setw(columnWidth) << std::right << BOLD(" Last Name") \
+			<< "|" << std::setw(columnWidth) << std::right << BOLD(" Nick Name") << std::endl;	
+		}
+
+		void printInfo() const{
+			const int columnWidth = 10;
+			std::cout << std::setw(columnWidth) << std::right << BOLD("     Index") \
+			<< "|" << std::setw(columnWidth) << std::right << BOLD("First Name") \
+			<< "|" << std::setw(columnWidth) << std::right << BOLD(" Last Name") \
+			<< "|" << std::setw(columnWidth) << std::right << BOLD(" Nick Name") << std::endl;	
+		}
 
 	private:
-		int	index;
-		int	contact;
 		std::string	firstName;
 		std::string	lastName;
-		std::string	nickname;
-}; */
-	#endif
+		std::string	nickName;
+}; 
+
+class PhoneBook {
+
+	public:
+		PhoneBook() : count(0), prints(0){
+			for (size_t i = 0; i < 8; i++)
+			{
+				contacts[i] = Contact("", "", "");
+				contacts[i].index = -1;
+			}
+		} 
+
+		void setContact(const Contact& contact) {
+			prints++;
+			if (count < 8)
+			{
+				contacts[count] = contact;
+				contacts[count].index = count;
+				count++;
+			}
+			else
+			{
+				count = 0;
+				contacts[count] = contact;
+				contacts[count].index = count;
+				count++;
+			}
+		}
+
+		void printContacts() const {
+			if (prints >= 7)
+			{
+				for (size_t i = 0; i < 8; i++)
+				{
+					if (i == 0)
+						contacts[i].displayHeader(); 
+					contacts[i].display(); 
+				}
+			}
+			else
+			{
+				for (size_t i = 0; i < prints; i++)
+				{
+					if (i == 0)
+						contacts[i].displayHeader(); 
+					contacts[i].display(); 
+				}
+			}
+		}
+		
+		void printContact(size_t i) const {
+			contacts[i].print_info();
+		}
+
+	private:
+		Contact contacts[8];
+		size_t count;
+		size_t prints;
+};
+
+#endif
